85195c0b09f8a68d3801e6d32536f10af5f0831e
Yurii Dzhumelia <uraniadz@gmail.com>
2024-01-15 10:30:38 +0200

    create state for BookForm

--- frontend/src/components/BooksLabrary/BookForm/BookForm.jsx
+++ frontend/src/components/BooksLabrary/BookForm/BookForm.jsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import './BookForm.css';
+
 function BookForm() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitle('');
+    setAuthor('');
+  };
   return (
     <>
       <div className="app-block book-form">
         <h2>Додати нову книгу</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="title">Назва книги: </label>
-            <input type="text" alt="" id="title" />
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              alt=""
+              id="title"
+            />
             <label htmlFor="author">Автор: </label>
-            <input type="text" alt="" id="author" />
+            <input
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              type="text"
+              alt=""
+              id="author"
+            />
           </div>
           <button type="submit">Додати книгу</button>
         </form>
